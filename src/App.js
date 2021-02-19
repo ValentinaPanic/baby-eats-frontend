3daf99d087c45c95f04be9677293e80a548aa004
ValentinaPanic <valentina.panic87@gmail.com>
2021-02-18 17:23:40 -0700

    Set redux and thunk in index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React, { Component } from 'react'
 
 
 
-function App() {
-  return (
-    <div >
-      well helloooo
-    </div>
-  );
+class App extends Component{
+ render(){
+    return (
+        <div >
+          well helloooo
+        </div>
+    );
+ 
+ }
+    
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose 
+const users = () => []
+const reducers = combineReducers({
+  users
+})
+const store = createStore(reducers, composeEnhancer(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
