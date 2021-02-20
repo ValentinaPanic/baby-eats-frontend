a0d9c70b9d196108033ae667433130b3aa5c7812
ValentinaPanic <valentina.panic87@gmail.com>
2021-02-19 19:53:45 -0700

    action creator for getCurrentUser

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,4 +1,4 @@
-export const serCurrentUser = user => {
+export const setCurrentUser = user => {
     return {
         type: "SET_CURRENT_USER",
         user
@@ -8,8 +8,10 @@ export const serCurrentUser = user => {
 
 // async creator
 export const login = credentials => {
+        console.log(credentials)
         return dispatch => {
             fetch("http://localhost:3001/login", {
+                credentials: "include",
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
@@ -17,7 +19,39 @@ export const login = credentials => {
                 body: JSON.stringify(credentials)
             })
             .then(response => response.json())
-            .then(console.log)
+            .then(user =>{
+                    if (user.error){
+                        alert(user.error)
+                    } else {
+                        dispatch(setCurrentUser(user))
+                    }
+            }
+            )
+
+        }
+
+    }
+
+    export const getCurrentUser = () => {
+       
+        return dispatch => {
+            fetch("http://localhost:3001/get_current_user", {
+                credentials: "include",
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+               
+            })
+            .then(response => response.json())
+            .then(user =>{
+                    if (user.error){
+                        alert(user.error)
+                    } else {
+                        dispatch(setCurrentUser(user))
+                    }
+            }
+            )
 
         }
 
