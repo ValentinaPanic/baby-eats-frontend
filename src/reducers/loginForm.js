846a771841d7a8c6f04f69b6d0f6335c8597c56d
ValentinaPanic <valentina.panic87@gmail.com>
2021-02-21 10:42:54 -0700

    reset fornm reducer

--- src/reducers/loginForm.js
+++ src/reducers/loginForm.js
@@ -8,6 +8,8 @@ const loginForm = (state = initialState, action) => {
     switch (action.type){
         case "UPDATE_LOGIN_FORM":
             return action.formData
+        case "RESET_FORM":
+            return initialState
         default:
             return state
     }
