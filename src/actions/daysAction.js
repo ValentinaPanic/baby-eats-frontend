a85ec02641a5173c74f71584c103ae7219e378e5
ValentinaPanic <valentina.panic87@gmail.com>
2021-02-25 21:15:06 -0700

    created days actions

---
+++ src/actions/daysAction.js
@@ -0,0 +1,76 @@
+export const setDays = days => {
+   
+    return {
+        type: "SET_DAYS",
+        days
+    }
+}
+export const addDay = day => {
+   
+    return {
+        type: "ADD_DAY",
+        day
+    }
+}
+export const clearDays =() => {
+    return {
+        type: "CLEAR_DAYS"
+    }
+}
+
+
+
+export const getDays = () => {
+    return dispatch => {
+        fetch("http://localhost:3001/days", {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+           
+        })
+        .then(response => response.json())
+        .then(data =>{
+            console.log(data )
+                if (data.error){
+                    alert(data.error)
+                } else {
+                    dispatch(setDays(data.data))
+                }
+            }
+        )
+    }
+}
+
+export const postDay = (dayData) => {
+    const dbDayData = {
+        day: {
+            user_id: dayData.userId,
+            date: dayData.date,
+            meal_type: dayData.mealType,
+            foods: dayData.foods
+
+        }
+    }
+    return dispatch => {
+        fetch("http://localhost:3001/days", {
+            credentials: "include",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+           body: JSON.stringify(dbDayData)
+        })
+        .then(response => response.json())
+        .then(data =>{
+            console.log(data )
+                // if (data.error){
+                //     alert(data.error)
+                // } else {
+                //     dispatch(addDay(data.data))
+                // }
+            }
+        )
+    }
+}
