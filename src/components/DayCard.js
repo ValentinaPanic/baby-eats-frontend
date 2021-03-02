a9d18450cbf6d08f25926d9acd83a3d4d4a0b107
ValentinaPanic <valentina.panic87@gmail.com>
2021-03-02 10:18:59 -0700

    pass day data as a prop to Food Cart

--- src/components/DayCard.js
+++ src/components/DayCard.js
@@ -1,25 +1,20 @@
  import React from "react";
  import { format } from "date-fns"
+import FoodCard from "./FoodCard";
 
 const DayCard = ({days, today}) => {
   const dayFormat = "yyyy-MM-dd"
   const formattedDate = format(today,dayFormat)
      return(
-          <div>
-              { days.days && days.days.filter(day => day.attributes.date ===     formattedDate).map( day => {
-                
-                return (day.attributes.foods.map(food =>{
-                
-                  return (
-                    <li key={day.id}> <strong>{food.meal_type}</strong> - {food.name}</li>
-                    )
-                }))
-              })
+        <div className="col col-center">
+           { days.days && days.days.filter(day => day.attributes.date === formattedDate).map( day => { 
+               return (day.attributes.foods.map(food =>{
+                  return <FoodCard food ={food}/> }
+             ))
+           })
                   
-                }
-    
-         
-    </div>
+          }
+        </div>
   
      )}
 
