ef307815eb6e79a5c4ecbf1a0be7874973dabb40
ValentinaPanic <valentina.panic87@gmail.com>
2021-03-02 10:18:22 -0700

    create Food Cart for rendering foof info

--- src/components/FoodCard.js
+++ src/components/FoodCard.js
@@ -1,1 +1,12 @@
-// 
+import React from 'react'
+
+const FoodCard = ({food}) => {
+    return (
+        <div>
+            <li key={food.id}> <strong>{food.meal_type}</strong> - {food.name} <span><button > Edit </button><button > Delete </button></span></li>
+                    
+        </div>
+    )
+}
+
+export default FoodCard
