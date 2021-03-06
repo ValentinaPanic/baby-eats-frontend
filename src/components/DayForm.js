7dfbbfb0846e77fb90de65712c84a4b15fcd9134
ValentinaPanic <valentina.panic87@gmail.com>
2021-03-06 10:33:07 -0700

    styled DayForm

--- src/components/DayForm.js
+++ src/components/DayForm.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 import { connect } from "react-redux"
+import {  withRouter} from 'react-router-dom';
 import { format } from "date-fns";
 import { createDay } from "../actions/dayForm"
 import { postDay } from "../actions/daysAction"
-const DayForm = ({dayData, createDay, postDay, userId, history}) => {
 
+const DayForm = ({dayData, createDay, postDay, userId, history}) => {
+console.log(history)
     const handleChange = (event) =>{
         const { name,value} = event.target 
         const updatedFormInfo = {
@@ -21,6 +24,7 @@ const DayForm = ({dayData, createDay, postDay, userId, history}) => {
             ...dayData,
             userId
         }, history)
+
     }
        
       const handleFoodsChange = event => {
@@ -39,7 +43,7 @@ const DayForm = ({dayData, createDay, postDay, userId, history}) => {
         const dateForm = format(dayData.date, dayFormat)
     return (
       <Form className="justify-content-md-center" inline onSubmit={handleSubmit}>
-        <Form.Group controlId="formGroupEmail">
+        <Form.Group >
            <Form.Control as='input' type="text" name="date" value={dateForm} onChange={handleChange}/>
            <Form.Control as="select" className="my-1 mr-sm-2" id="inlineFormCustomSelectPref" custom
                 name="mealType" value={dayData.foods.mealType} onChange={handleFoodsChange}>
@@ -51,7 +55,9 @@ const DayForm = ({dayData, createDay, postDay, userId, history}) => {
 
             
             <Form.Control as="input" type="text" name="name" value={dayData.foods.name} onChange={handleFoodsChange}/>  
-            <Form.Control as="input" type="submit"className="mb-2" variant="info"/>   
+            <Button variant="primary" type="submit">
+             Create
+            </Button>
          </Form.Group>
         </Form>
     )
@@ -64,4 +70,4 @@ const mapStateToProps = state =>{
     }
 }
 
-export default connect(mapStateToProps, {createDay, postDay})(DayForm)
+export default withRouter(connect(mapStateToProps, {createDay, postDay})(DayForm))
