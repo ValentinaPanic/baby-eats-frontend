f5961e0c1378c15bfe0a6642193e5137140c980b
ValentinaPanic <valentina.panic87@gmail.com>
2021-03-07 14:24:53 -0700

    comment out the component until used

--- src/containers/NewDayFormContainer.js
+++ src/containers/NewDayFormContainer.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import { connect } from "react-redux"
-import { postDay } from "../actions/daysAction"
+// import React from 'react'
+// import { connect } from "react-redux"
+// import { postDay } from "../actions/daysAction"
 
-import DayForm from '../components/DayForm'
+// import DayForm from '../components/DayForm'
 
-const NewDayFomContainer = ({postDay, history}) => {
+// const NewDayFomContainer = ({postDay, history}) => {
    
-    const handleSubmit = (event, dayData, userId) => {
-        event.preventDefault()
+//     const handleSubmit = (event, dayData, userId) => {
+//         event.preventDefault()
         
-        postDay({
-            ...dayData,
-            userId
-        }, history)
-        history.push('/')
-    }  
-  return  <DayForm history={history} handleSubmit={handleSubmit}/>   
+//         postDay({
+//             ...dayData,
+//             userId
+//         }, history)
+//         history.push('/')
+//     }  
+//   return  <DayForm history={history} handleSubmit={handleSubmit}/>   
     
     
-}
+// }
 
-export default connect(null, { postDay })(NewDayFomContainer)
+// export default connect(null, { postDay })(NewDayFomContainer)
